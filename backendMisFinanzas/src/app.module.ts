db297abd0bdc403074b9e59d60d300c80411a225
juanPabloCesarini <juanpcesarini@hotmail.com>
2025-12-19 08:44:05 -0300

    configuración de docker y variables de entorno

--- backendMisFinanzas/src/app.module.ts
+++ backendMisFinanzas/src/app.module.ts
@@ -3,17 +3,38 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoriesModule } from './categories/categories.module';
 import { SubcategoriesModule } from './subcategories/subcategories.module';
-import { MovementTypeModule } from './movement_type/movement_type.module';
-import { AccountModule } from './account/account.module';
-import { MovementModule } from './movements/movement.module';
 import { MovementsModule } from './movements/movements.module';
 import { AccountsModule } from './accounts/accounts.module';
 import { MovementTypesModule } from './movement_types/movement_types.module';
 import { AuthModule } from './auth/auth.module';
 import { SeedModule } from './seed/seed.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CategoriesModule, SubcategoriesModule, MovementTypeModule, AccountModule, MovementModule, MovementsModule, AccountsModule, MovementTypesModule, AuthModule, SeedModule],
+  imports: [
+    CategoriesModule,
+    SubcategoriesModule,
+    MovementsModule,
+    AccountsModule,
+    MovementTypesModule,
+    AuthModule,
+    SeedModule,
+    ConfigModule.forRoot({
+      isGlobal: true, // disponible en toda la app
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mariadb',
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT ? +process.env.DB_PORT : 3310,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD || '',
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+      logging: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- backendMisFinanzas/src/main.ts
+++ backendMisFinanzas/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const logger = new Logger('main');
+
+  app.enableCors({ origin: '*' }); // ajustar origen en producción
+
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  
+
+  await app.listen(process.env.port ?? 3000);
+  logger.log(`App running in port ${process.env.port}`);
 }
 bootstrap();
